1bbc5d47f0caa4e0be7bc43ad6b8788d0ecae3c7
Wallace Correa <wallace.correa@logicatecnologia.com.br>
2021-04-07 16:41:30 -0300

    Commit inicial atualizado

---
+++ src/JogodaVelha/Jogador.java
@@ -0,0 +1,37 @@
+package JogodaVelha;
+
+public abstract class Jogador {
+    
+    protected int[] tentativa = new int[2];
+    protected int jogador;
+
+	public int Dificuldade;
+	
+    public int getDificuldade() {
+		return Dificuldade;
+	}
+
+	public void setDificuldade(int dificuldade) {
+		Dificuldade = dificuldade;
+	}    
+    
+    public Jogador(int jogador){
+        this.jogador = jogador;
+    }
+    
+    public abstract void jogar(Tabuleiro tabuleiro);
+    
+    public abstract void Tentativa(Tabuleiro tabuleiro);
+
+    public boolean checaTentativa(int[] tentativa, Tabuleiro tabuleiro){
+        if(tabuleiro.getPosicao(tentativa) == 0)
+            return true;
+        else
+            return false;
+            
+    }
+    
+    
+    		
+    
+}
